26636a8554df24d8e0ebd056777eaf522c9b9164
TETSU <kaoru6strings@gmail.com>
2019-10-10 18:28:27 +0900

    Remove Repository pattern

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,26 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart;
 use App\Order;
-use App\Repository\CartRepository;
-use App\Repository\UserRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
-    private $userRepository;
-    private $cartRepository;
-    private $order;
-
-    public function __construct(UserRepository $userRepository, CartRepository $cartRepository, Order $order)
-    {
-        $this->userRepository = $userRepository;
-        $this->cartRepository = $cartRepository;
-        $this->order = $order;
-    }
-
     public function store(Request $request)
     {
         try {
@@ -29,25 +17,25 @@ class OrderController extends Controller
             $loginUser = Auth::user();
 
             # カートからデータを取得
-            $cartItem =  collect($this->cartRepository->find($loginUser->id));
+            $cartItem =  collect(Cart::find($loginUser->id));
 
             if ($cartItem->isEmpty()) {
                 return redirect(route('cart.index'))->with('error', 'カートの中に商品がありません。');
             }
 
             # 商品の在庫チェック
-            if ($this->order->check($cartItem)) {
+            if (Order::check($cartItem)) {
                 return redirect(route('cart.index'))->with('error', '商品の在庫がない商品がありました。');
             }
 
             # 注文確定情報を保存
-            $this->order->store($loginUser, $cartItem);
+            Order::store($loginUser, $cartItem);
 
             # 金額を計算
-            $this->order->calculateAmount();
+            Order::calculateAmount();
 
             # クレジットカード決済
-            $result = $this->order->pay();
+            $result = Order::pay();
 
             if ($result === false) {
                 return redirect(route('cart.index'))->with('error', 'カード決済に失敗しました');
